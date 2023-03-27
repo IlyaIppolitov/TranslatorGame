ec395572771cdbfd52e307abed75a64d28370c22
FinT08 <IppolitovIS@gmail.com>
2023-03-27 09:32:08 +0300

    Nuget packet system.speech installed

--- TranslatorGame/MainWindow.xaml.cs
+++ TranslatorGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Speech.Recognition;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -7,8 +8,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Speech;
-using System.Speech.Recognition;
 
 namespace TranslatorGame
 {
