452086b38c3c3c52f2e4d882e153c889b0610975
Khristina <ptykhina.khristi@mail.ru>
2023-04-02 16:06:24 +0400

    Корректировка названий

--- TranslatorGame/AppDbContext.cs
+++ TranslatorGame/AppDbContext.cs
@@ -8,15 +8,14 @@ namespace TranslatorGame
 {
     public class AppDbContext : DbContext
     {
-        //private const string dataBaseName = "LanguageGames.db";
-        private const string dataBaseName = @"Data Source = C:\Users\79053\Desktop\Курсовая\TranslatorGame\LanguageGames.db";
+        private const string dataBaseName = "LanguageGames.db";     
         protected override void OnConfiguring(
             DbContextOptionsBuilder optionsBuilder)
         {
             var appDir = Path.GetFullPath(Directory.GetCurrentDirectory() + "\\..");
             //var appDir = "D:\\ITStep\\CSharp\\EFCore\\TranslatorGame";
-            //var ConnectionString = $"Data Source = {appDir}\\{dataBaseName}";
-            optionsBuilder.UseSqlite(dataBaseName);
+            var ConnectionString = $"Data Source = {appDir}\\{dataBaseName}";
+            optionsBuilder.UseSqlite(ConnectionString);
         }
 
         public DbSet<Player> Players => Set<Player>();
